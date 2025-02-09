18a101d07c50f0d4df0f3140dbaa57c2aaa4daad
SeongWonKim <kssw888@gmail.com>
2025-02-09 20:09:00 +0900

    update main

--- lightweight-resnet/main.py
+++ lightweight-resnet/main.py
@@ -3,7 +3,7 @@ from helper.trainer import Trainer
 from helper.loader import Loader
 from models.resnet50 import get_resnet50
 from helper.data_generator import cifar100
-from helper.evaluator import Evaluator
+from helper.evaluator import Evaluator, number_of_parameters
 from modules.distillation_loss import DistillationLoss
 from modules.pruner import Pruner
 
@@ -13,17 +13,27 @@ if __name__ == '__main__':
     loader = Loader(device)
     evaluator = Evaluator(device)
     
-    model = get_resnet50(100).to(device)
-    loader.model_load('check_points/original', model)
+    # Load dataset
+    train_data_loader = cifar100(path='./datasets/train', batch_size=256, train=True)
+    test_data_loader = cifar100(path='./datasets/test', batch_size=256, train=False)
     
-    train_data_loader = cifar100(path = './datasets/train', batch_size=256)
-    test_data_loader = cifar100(path = './datasets/test', batch_size=256)
+    # Load a pre-trained model on ImageNet and fine-tune it
+    original = get_resnet50(100)
+    trainer = Trainer(original, torch.nn.CrossEntropyLoss())
+    trainer.train(train_data_loader, test_data_loader, 100, 'check_points/original', 'runs/original')
     
-    pruner = Pruner(model, train_data_loader)
-    pruner.iterative_prune(10, test_data_loader, 20)
+    # Load the teacher model for Knowledge Distillation
+    teacher = loader.model_load('check_points/original')
+    model = loader.model_load('check_points/original')
     
-    #evaluator.evaluate(model, test_data_loader, True)
-    #trainer = Trainer(model, torch.nn.CrossEntropyLoss())
-    #trainer.train(train_data_loader, test_data_loader, 300, 'check_points/original', 'runs/original')
+    # Perform iterative pruning followed by Knowledge Distillation retraining
+    pruner = Pruner(teacher, model)
+    pruner.iterative_prune(20, train_data_loader, test_data_loader, 30)
     
+    # Measure the number of parameters and accuracy after all processes
+    number_of_parameters(model, print_result=True)    
+    evaluator.evaluate(model, test_data_loader, True)
     
+    # Retrain the pruned model if necessary
+    trainer = Trainer(model, DistillationLoss(5.0, 0.7))
+    trainer.distillation_train(teacher, train_data_loader, test_data_loader, 50, 'check_points/after_pruning', 'runs/after_pruning')
